f10cbb338882add941f1b99d414affbc0f2c720a
Fridalindbjerg <fridalindbjerg@outlook.dk>
2026-05-22 12:26:37 +0200

    fix: add padding to title and description in CaseItem component for improved layout

--- actum/src/app/[locale]/(routes)/cases/components/case_item.tsx
+++ actum/src/app/[locale]/(routes)/cases/components/case_item.tsx
@@ -54,11 +54,13 @@ export default function CaseItem({ caseItem }: Props) {
           />
         )}
       </motion.div>
-      <div className="col-[content-start/content-end] md:col-[4/6] self-center md:pl-6">
+      <div className="col-[content-start/content-end] md:col-[4/6]   self-center md:pl-6">
         {/* Henter den lokaliserede titel fra jsonb-objektet baseret på aktiv locale */}
-        <p>{caseItem.title[locale]}</p>
+        <p className="pt-6 pb-4 ">{caseItem.title[locale]}</p>
         {/* Henter den lokaliserede beskrivelse fra jsonb-objektet baseret på aktiv locale */}
-        <motion.p style={{ opacity, y }}>{caseItem.description[locale]}</motion.p>
+        <motion.p className="pb-6" style={{ opacity, y }}>
+          {caseItem.description[locale]}
+        </motion.p>
       </div>
     </article>
   );
